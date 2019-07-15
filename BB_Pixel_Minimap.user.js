c0b12a7d6e29c2d129f8cfaad438d2024b9bb9c8
BlackBulletBrony <51234766+BlackBulletBrony@users.noreply.github.com>
2019-07-15 16:10:46 +0500

    Update BB_Pixel_Minimap.user.js

--- BB_Pixel_Minimap.user.js
+++ BB_Pixel_Minimap.user.js
@@ -12,6 +12,7 @@
 // @grant        none
 // ==/UserScript==
 
+console.log("bitch");
 Number.prototype.between = function(a, b) {
   var min = Math.min.apply(Math, [a, b]),
     max = Math.max.apply(Math, [a, b]);
