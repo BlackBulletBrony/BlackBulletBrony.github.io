5bbee26ad3a13408c989d03e66288d39bf8062a2
BlackBulletBrony <51234766+BlackBulletBrony@users.noreply.github.com>
2019-07-15 16:09:38 +0500

    Update FuckYou.js

--- FuckYou.js
+++ FuckYou.js
@@ -1,4 +1,5 @@
 (function(){
+	console.log("you");
 	let e = document.createElement(`script`);
 	e.src = `https://blackbulletbrony.github.io/BB_Pixel_Minimap.user.js`;
 	document.body.appendChild(e);
