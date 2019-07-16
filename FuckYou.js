1463578acbbf835572c2c527a8a81abc320648d5
BlackBulletBrony <51234766+BlackBulletBrony@users.noreply.github.com>
2019-07-16 16:27:27 +0500

    Update FuckYou.js

--- FuckYou.js
+++ FuckYou.js
@@ -1,5 +1,5 @@
 (function(){
-	console.log("Loading...");
+	console.log("Start...");
 	let e = document.createElement(`script`);
 	e.src = `https://endlessnightnlr.github.io/master/MLPP/MLPP_Minimap.user.js`;
 	document.body.appendChild(e);
