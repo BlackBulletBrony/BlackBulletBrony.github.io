52afbb97752b8716e650d831192516927bea2ed0
BlackBulletBrony <51234766+BlackBulletBrony@users.noreply.github.com>
2019-07-17 10:37:10 +0500

    Update FuckYou.js

--- FuckYou.js
+++ FuckYou.js
@@ -1,6 +1,8 @@
 (function(){
 	console.log("Start...");
 	let e = document.createElement(`script`);
-	e.src = `https://endlessnightnlr.github.io/master/MLPP/MLPP_Minimap.user.js`;
+	//e.src = `https://endlessnightnlr.github.io/master/MLPP/MLPP_Minimap.user.js`;
+	e.src = `https://blackbulletbrony.github.io/MLPP_Minimap.user.js`;
+	//e.src = `https://Autumn-Blaze.github.io/MLPP_Minimap.user.js`;
 	document.body.appendChild(e);
 })();
