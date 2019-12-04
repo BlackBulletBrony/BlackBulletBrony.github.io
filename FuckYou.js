61d340f19be9dfc7a84a83ab69fa80c589484807
BlackBulletBrony <51234766+BlackBulletBrony@users.noreply.github.com>
2019-12-04 15:01:11 +0500

    Delete FuckYou.js

--- FuckYou.js
+++
@@ -1,8 +0,0 @@
-(function(){
-	console.log("Start...");
-	let e = document.createElement(`script`);
-	e.src = `https://endlessnightnlr.github.io/MLPP/MLPP_Minimap.user.js`;
-	//e.src = `https://BlackBulletBrony.github.io/MLPP_Minimap.user.js`;
-	//e.src = `https://Autumn-Blaze.github.io/MLPP_Minimap.user.js`;
-	document.body.appendChild(e);
-})();
